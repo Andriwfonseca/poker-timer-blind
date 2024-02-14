b75cf56376f0636e7c805a5e1d32e216c41a179b
Andriw Fonseca <filipefonsequinha@gmail.com>
2024-02-14 19:51:24 -0300

    feat: criado controle do blind

---
+++ assets/js/script.js
@@ -0,0 +1,75 @@
+let started = false;
+let interval;
+
+function startTimer() {
+    const timer = $("#timer");
+    let minute = timer.text().split(":")[0];
+    let second = timer.text().split(":")[1];
+
+    started = true;
+
+    interval = setInterval(() => {
+        if (started) {
+            if (second > 0) {
+                second -= 1;
+            } else {
+                if (minute > 0) {
+                    minute -= 1;
+                    second = 59;
+                } else {
+                    callAlarm();
+                }
+            }
+        } else {
+            second = 0;
+        }
+
+        timer.text(minute + ":" + second);
+    }, 5);
+}
+
+function callAlarm() {
+    clearInterval(interval);
+    started = false;
+
+    openModal();
+}
+
+function closeAlarm() {
+    toBendBlind();
+
+    $("#timer").text("15:00");
+
+    closeModal();
+}
+
+function openModal() {
+    $("#modal").show();
+}
+
+function closeModal() {
+    $("#modal").hide();
+}
+
+function pauseTimer() {
+    clearInterval(interval);
+}
+
+function stopTimer() {
+    clearInterval(interval);
+    $("#timer").text("15:00");
+}
+
+function toBendBlind() {
+    const blinds = $("#values-blinds");
+    let smallBlind = blinds.text().split("/")[0];
+    let bigBlind = blinds.text().split("/")[1];
+
+    const newBlinds = smallBlind * 2 + "/" + bigBlind * 2;
+    blinds.text(newBlinds);
+}
+
+function resetBlind() {
+    const blinds = $("#values-blinds");
+    blinds.text("25/50");
+}
